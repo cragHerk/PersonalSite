4f2aa7614a34516f97101a4d68fa236bf58d919c
m.backi <108221259+cragHerk@users.noreply.github.com>
2023-11-27 16:00:38 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import ShuffleHero from "./Components/Hero/ShuffleHero";
 import ChipTabs from "./Components/Nav/ChipTabs";
 import { BouncyCardsFeatures } from "./Components/BouncyCard/BouncyCardFeatures";
-
 import LogoFooter from "./Components/Footer/LogoFooter";
 import Portfolio from "./Components/Portfolio/Portfolio";
 import Contact from "./Components/Contact/Contact";
@@ -13,11 +13,16 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
       </Routes>
     </Router>
   );
 }
 const Home = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <>
       <ChipTabs />
