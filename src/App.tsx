f2da82831126e7d2c7203cde723f1fbeec8f7734
m.backi <108221259+cragHerk@users.noreply.github.com>
2023-11-27 16:22:29 +0100

    suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import { Suspense } from "react";
+import { Suspense, useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import ShuffleHero from "./Components/Hero/ShuffleHero";
 import ChipTabs from "./Components/Nav/ChipTabs";
 import { BouncyCardsFeatures } from "./Components/BouncyCard/BouncyCardFeatures";
-
 import LogoFooter from "./Components/Footer/LogoFooter";
 import Portfolio from "./Components/Portfolio/Portfolio";
 import Contact from "./Components/Contact/Contact";
+import { SuspenseSpinner } from "./Components/SuspenseSpinnner/SuspenseSpinner";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <Router>
       <Routes>
         <Route
           path="/"
           element={
-            <Suspense fallback={<div>Loading...</div>}>
-              <Home />
-            </Suspense>
+            isLoading ? (
+              <SuspenseSpinner />
+            ) : (
+              <Suspense fallback={<SuspenseSpinner />}>
+                <Home />
+              </Suspense>
+            )
           }
         />
         <Route path="*" element={<Home />} />
---
+++ src/Components/SuspenseSpinnner/SuspenseSpinner.tsx
@@ -0,0 +1,8 @@
+import { ClimbingBoxLoader } from "react-spinners";
+export const SuspenseSpinner = () => {
+  return (
+    <div className="fixed h-full w-full flex justify-center items-center bg-custom-black">
+      <ClimbingBoxLoader color="red" size={50} />
+    </div>
+  );
+};
