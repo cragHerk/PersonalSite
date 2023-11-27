9051c78ef0f5e9b050407b87e2cb75facedcfa64
m.backi <108221259+cragHerk@users.noreply.github.com>
2023-11-27 16:11:20 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { useEffect } from "react";
+import { Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import ShuffleHero from "./Components/Hero/ShuffleHero";
 import ChipTabs from "./Components/Nav/ChipTabs";
 import { BouncyCardsFeatures } from "./Components/BouncyCard/BouncyCardFeatures";
+
 import LogoFooter from "./Components/Footer/LogoFooter";
 import Portfolio from "./Components/Portfolio/Portfolio";
 import Contact from "./Components/Contact/Contact";
@@ -12,17 +13,20 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Home />
+            </Suspense>
+          }
+        />
         <Route path="*" element={<Home />} />
       </Routes>
     </Router>
   );
 }
 const Home = () => {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-
   return (
     <>
       <ChipTabs />
